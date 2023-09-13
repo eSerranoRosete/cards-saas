1c7a8dfbfcd75e79659f6944a788688c342075af
eserranor98 <eduardo.serrano@dovela.com>
2023-09-12 23:02:17 -0600

    updated env variables

--- firebase/firebaseApp.ts
+++ firebase/firebaseApp.ts
@@ -8,13 +8,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC8LWiFC41sEralfdNK8zi0iTzXeOyAQuw",
-  authDomain: "inteminer-26db4.firebaseapp.com",
-  projectId: "inteminer-26db4",
-  storageBucket: "inteminer-26db4.appspot.com",
-  messagingSenderId: "1009237995097",
-  appId: "1:1009237995097:web:0f3998c26e1a218cb4cb48",
-  measurementId: "G-TSV2MGFN3Z",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase

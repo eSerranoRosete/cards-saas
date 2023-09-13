61b3af186a2f84fa276b29ac4b8b15b6a37882de
eserranor98 <eduardo.serrano@dovela.com>
2023-09-13 14:20:46 -0600

    updated carousel styles

--- components/application/card/CardCarousel.tsx
+++ components/application/card/CardCarousel.tsx
@@ -16,7 +16,7 @@ export const CardCarousel = ({ items }: Props) => {
   if (items.length === 0) return <></>;
 
   return (
-    <div className="w-full h-80 relative gap-4 flex snap-x snap-mandatory overflow-auto ">
+    <div className="w-full h-full relative gap-4 flex snap-x snap-mandatory overflow-auto ">
       {items.map((item, i) => {
         const isImgOnly = !item.title && !item.description;
 
@@ -31,7 +31,7 @@ export const CardCarousel = ({ items }: Props) => {
             as="a"
             {...(item.url ? linkProps : {})}
             isPressable={!!item.url}
-            className="w-10/12 snap-always relative first:ml-10 last:mr-10  max-w-xs h-full p-2 flex flex-col rounded-large snap-center shrink-0"
+            className="w-10/12 snap-always relative gap-2 first:ml-10 last:mr-10 max-w-xs h-80 p-2 flex flex-col rounded-large snap-center shrink-0"
           >
             {item.url && (
               <Button
@@ -43,22 +43,29 @@ export const CardCarousel = ({ items }: Props) => {
                 <ExternalLink className="w-3" />
               </Button>
             )}
-            <Image
-              removeWrapper
-              className={cn(
-                "w-full h-full object-cover",
-                !isImgOnly && "max-h-[70%]"
-              )}
-              src={item.img.url}
-              alt={item.title || ""}
-            />
 
+            <div className="w-full h-full relative grow">
+              <Image
+                removeWrapper
+                className={cn("w-full object-cover absolute h-full")}
+                src={item.img.url}
+                alt={item.title}
+              />
+            </div>
             {!isImgOnly && (
-              <div className="p-2 py-4 h-full text-left">
-                <p className="font-semibold w-full inline-block ">
-                  {item.title}
-                </p>
-                <p className="text-sm text-default-500">{item.description}</p>
+              <div className="w-full h-auto px-2">
+                <div className="text-left">
+                  {item.title && (
+                    <p className="font-semibold w-full inline-block ">
+                      {item.title}
+                    </p>
+                  )}
+                  {item.description && (
+                    <p className="text-sm text-default-500">
+                      {item.description}
+                    </p>
+                  )}
+                </div>
               </div>
             )}
           </Card>

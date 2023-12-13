35b180932c8bdb9eba726f03c7994401423e4fd5
eserranor98 <eduardo.serrano@dovela.com>
2023-12-12 20:50:21 -0600

    dark mode to html

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -7,7 +7,7 @@ import { SessionProvider } from "next-auth/react";
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
-      <Theme appearance="dark">{children}</Theme>
+      <Theme>{children}</Theme>
       <Toaster />
     </SessionProvider>
   );
